85834c4cc95aac8caeb927d8e48d7e0d17000752
Maykmora <maykmora12@gmail.com>
2025-07-16 14:08:12 -0600

    Menu principal

--- Actividad 3 en grupo.py
+++ Actividad 3 en grupo.py
@@ -1,2 +1,7 @@
 #Maynor Eduardo Morales Chang- 1553025
 
+print("Bienvenido a el programa de conversion de unidades de medida")
+print("1.Conversion de unidades de temperatura")
+print("2.Conversion de unidades de longitud")
+
+
