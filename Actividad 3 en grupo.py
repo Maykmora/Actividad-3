4fbfb4a26b611924dd79d780c36ed13d57adaa77
Maykmora <maykmora12@gmail.com>
2025-07-16 14:27:17 -0600

    commit 3

--- Actividad 3 en grupo.py
+++ Actividad 3 en grupo.py
@@ -7,15 +7,30 @@ while True:
     op=int(input("Ingrese el numero de opcion que desea seleccionar:"))
     if op==1:
         while True:
+            print()
             print("1.Convertir Fahrenheit a Celsius")
             print("2.Convertir de Celsius a Fahrenheit")
             print("3.salir")
             op2=int(input("Ingrese el numero de opcion que desea seleccionar:"))
             if op2==1:
                 fa=int(input("Ingrese la cantidad de grados Fahrenheit para convertir:"))
+                conv=(fa-32)/1.8
+                print(f"{fa} grados Fahrenheit es equivalente a {conv} grados Celcius")
 
-            if op2==2:
+            elif op2==2:
                 ce=int(input("Ingrese la cantidad de grados Celsius para convertir:"))
+            elif op2==3:
+                print("Regresando al menú")
+                break
+            else:
+                print("Ingrese valores validos")
+                break
+    elif op==2:
+        while True:
+            print("1.Convertir de Kilometros a Metros")
+            print("2.Convertir de Metros a Kilometros")
+
+
 
 
 
