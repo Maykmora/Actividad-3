7be63e4cc18818e0b9f493402937ebcce08f9f77
Maykmora <maykmora12@gmail.com>
2025-07-16 17:02:41 -0600

    corrigiendo errores 2

--- Actividad 3 en grupo.py
+++ Actividad 3 en grupo.py
@@ -1,7 +1,7 @@
 #Maynor Eduardo Morales Chang- 1553025
 #Santos Juan Diego Chuc Gutiérrez - 1532825
 while True:
-    print("Bienvenido a el programa de conversion de unidades de medida")
+    print("Bienvenido al programa de conversion de unidades de medida")
     print("1.Conversion de unidades de temperatura")
     print("2.Conversion de unidades de longitud")
     print("3.Salir")
@@ -11,21 +11,21 @@ while True:
             print()
             print("1.Convertir Fahrenheit a Celsius")
             print("2.Convertir de Celsius a Fahrenheit")
-            print("3.salir")
+            print("3.Salir")
             op2=int(input("Ingrese el numero de opcion que desea seleccionar:"))
             if op2==1:
                 fa=float(input("Ingrese la cantidad de grados Fahrenheit para convertir:"))
                 conv=(fa-32)/1.8
-                print(f"{fa} grados Fahrenheit es equivalente a {conv} grados Celcius")
+                print(f"{fa} grados Fahrenheit es equivalente a {conv:.2f} grados Celsius")
             elif op2==2:
                 ce=float(input("Ingrese la cantidad de grados Celsius para convertir:"))
                 conv=(ce*1.8)+32
-                print(f"{ce} grados Fahrenheit es equivalente a {conv} grados Celcius")
+                print(f"{ce} grados Celsius es equivalente a {conv:.2f} grados Fahrenheit")
             elif op2==3:
                 print("Regresando al menú")
                 break
             else:
-                print("Ingrese valores validos")
+                print("Ingrese valores válidos")
     elif op==2:
         while True:
             print("1.Convertir de Kilometros a Metros")
@@ -33,18 +33,18 @@ while True:
             print("3.Salir")
             op3=int(input("Ingrese el numero de opcion que desea seleccionar:"))
             if op3==1:
-                km=int(input("Ingrese la cantidad de Kilometros que desea convertir a Metros:"))
+                km=float(input("Ingrese la cantidad de Kilometros que desea convertir a Metros:"))
                 conv= (km*1000)
-                print(f"{km} Kilometros es equivalente a {conv} Metros")
+                print(f"{km} Kilometros es equivalente a {conv:.2f} Metros")
             elif op3 ==2:
-                metros =int(input("Ingrese la cantidad de Metros que desea convertir a Kilometros:"))
+                metros =float(input("Ingrese la cantidad de Metros que desea convertir a Kilometros:"))
                 conv= (metros/1000)
-                print(f"{metros} Metros es equivalente a {conv} Kilometros")
+                print(f"{metros} Metros es equivalente a {conv:.2f} Kilometros")
             elif op3 ==3:
-                print("Regresando al menu")
+                print("Regresando al menú")
                 break
             else:
-                print("Ingrese valores validos")
+                print("Ingrese valores válidos")
 
     elif op==3:
         print("Muchas gracias por haber usado el programa, feliz día")
