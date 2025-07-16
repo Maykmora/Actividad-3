3ba4c3a21d94e7cf6e3032e2cea4d300e6d3f25d
Maykmora <maykmora12@gmail.com>
2025-07-16 14:11:01 -0600

    commit 1

--- Actividad 3 en grupo.py
+++ Actividad 3 en grupo.py
@@ -1,7 +1,10 @@
 #Maynor Eduardo Morales Chang- 1553025
+while True:
+    print("Bienvenido a el programa de conversion de unidades de medida")
+    print("1.Conversion de unidades de temperatura")
+    print("2.Conversion de unidades de longitud")
+    op=int(input("Ingrese el numero de opcion que desea seleccionar:"))
 
-print("Bienvenido a el programa de conversion de unidades de medida")
-print("1.Conversion de unidades de temperatura")
-print("2.Conversion de unidades de longitud")
+
 
 
