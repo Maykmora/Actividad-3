a659f750e9b48d9f23214cede3c41647cf8d29c6
Maykmora <maykmora12@gmail.com>
2025-07-16 14:13:33 -0600

    commit 2

--- Actividad 3 en grupo.py
+++ Actividad 3 en grupo.py
@@ -3,7 +3,13 @@ while True:
     print("Bienvenido a el programa de conversion de unidades de medida")
     print("1.Conversion de unidades de temperatura")
     print("2.Conversion de unidades de longitud")
+    print("3.Salir")
     op=int(input("Ingrese el numero de opcion que desea seleccionar:"))
+    if op==1:
+        while True:
+            print("1.Convertir Fahrenheit a Celsius")
+            print("2.Convertir de Celsius a Fahrenheit")
+            print("3.salir")
 
 
 
